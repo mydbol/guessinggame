886f75ed4351746eb1527712b916d1e36f755d79
mydbol <kobrien@mydbol.com>
2015-12-12 23:49:02 -0500

    created guessing game repo

---
+++ jscript/framworks/angular/myAppMain.js
@@ -0,0 +1,33 @@
+var myAppMain=angular.module('myAppMain',['ngRoute','myAppControllers','ngCookies','UserAuthFactory','kobServices','MenuStandard']);
+/*
+myapplication.config(['$routeProvider',
+                        function($routeProvider){
+                            $routeProvider.
+                            when('/page1/:param',{
+                                        templateUrl:'/partials/page1.html',
+                                        controller: 'page1'
+                                    }).
+                            when('/page2/:param',{
+                                        templateUrl:'/partials/page1.html',
+                                        controller: 'page2'
+                                    }).
+                            when('/page3/:param',{
+                                        templateUrl:'/partials/page1.html',
+                                        controller: 'page3'
+                                    }).
+                            when('//page4/:param',{
+                                        templateUrl:'/partials/page1.html',
+                                        controller: 'page4'
+                                    }).
+                            when('//page5/:param',{
+                                        templateUrl:'/partials/page1.html',
+                                        controller: 'page5'
+                                    }).
+                            otherwise({
+                                        redirectTo:'/home/0',
+                                     
+                            });
+                        }
+                    ]);
+ 
+ */
---
+++ jscript/guessinggame.js
@@ -0,0 +1,5 @@
+//create a countdowm clock that change teh count down interval base on hot/coldness
+//if teh user fails it should blow up 
+// the they guess that save the world ()
+// hit should be within 5 of the answer.
+// strikes should be denoted by swelly dog turd.
